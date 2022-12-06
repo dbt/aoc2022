d9165bb9cfad49e1e51ef5770b7f965fb1593b4d
David Terrell <dterrell@gmail.com>
2022-12-06 01:14:19 -0800

    move things into adventools

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 use advent2022::days::all_days;
+use adventools::runner;
 use anyhow::Result;
-use std::time::Instant;
 
 fn main() -> Result<()> {
-    for d in all_days() {
-        {
-            print!("Day {} part 1: ", d.number());
-            let i1 = Instant::now();
-            let res1 = d.part01();
-            let dur1 = i1.elapsed();
-            println!("Elapsed time: {:?}", dur1);
-            res1?;
-            println!()
-        }
-
-        print!("Day {} part 2: ", d.number());
-        let i2 = Instant::now();
-        let res2 = d.part02();
-        let dur2 = i2.elapsed();
-        println!("Elapsed time: {:?}", dur2);
-        res2?;
-        println!()
-    }
-    Ok(())
+    runner::run(all_days())
 }
